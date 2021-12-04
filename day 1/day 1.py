0ef82442c76de1b86e4eb12ddf372d2dcb521254
Scott Ellis <scottellis94@gmail.com>
2021-12-04 00:26:35 -0500

    input file

--- day 1/day 1.py
+++ day 1/day 1.py
@@ -1,3 +1,10 @@
-fileObject = open("input.txt", "r")
+with open('day 1/input.txt') as f:
+    lines = f.readlines()
 
-testlist = fileObject.readline()
+depths = [int(x) for x in lines]
+count = 0
+
+for n, x in enumerate(depths):
+    if x > depths[n-1]:
+        count += 1
+print(count)
