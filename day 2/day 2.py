24c45825da7b3af1de9b0dca1fd87dc08aa85b44
Scott Ellis <scottellis94@gmail.com>
2021-12-04 02:11:43 -0500

    trying

--- day 2/day 2.py
+++ day 2/day 2.py
@@ -8,14 +8,21 @@ move_dict = {
     "down": "y+",
     "up": "y-"
 }
-
+depth = 0
+horiz = 0
 x = 0
 y = 0
+aim = 0
 
 for n in lines:
     if move_dict[n[:-2]] == 'x+':
         x = eval(str(move_dict[n[:-2]]) + str(n[-1]))
-    else:
-        y = eval(str(move_dict[n[:-2]]) + str(n[-1]))
+        horiz = horiz + int(n[-1])
+    elif move_dict[n[:-2]] == 'y+':
+        aim += 5
+    elif move_dict[n[:-2]] == 'y-':
+        aim -= 5
+    depth = horiz * aim
+print(horiz*depth)
 
-print(x*y)
+
