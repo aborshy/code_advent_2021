5423b3a910406e4af9fd0925dfe8bb8254c49b87
Scott Ellis <scottellis94@gmail.com>
2021-12-07 00:26:36 -0500

    day 1 part 2

--- day 1/day 1 part 2.py
+++ day 1/day 1 part 2.py
@@ -4,9 +4,17 @@ with open('input.txt') as x:
 depths = [int(x) for x in lines]
 count = 0
 
-for n, x in enumerate(depths):
-    for a in range(0,2):
-        rolling_total=x[n] + x[n+1] + x[n+2]
-        count += 1
+# list comp for each sliding pattern
+sums = [[depths[n], depths[n+1], depths[n+2]] for n, x in enumerate(depths[:-2])]
 
+# appending last two indexes because i'm lazy
+sums.append([depths[-2],depths[-1]])
+sums.append([depths[-1]])
+
+for n, x in enumerate(sums):
+    if sum(x) > sum(sums[n-1]):
+        count += 1
 print(count)
+
+
+
