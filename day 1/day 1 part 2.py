08f77dcc388134ad61c7cee60f1d30b05c70e544
Scott Ellis <scottellis94@gmail.com>
2021-12-07 00:29:05 -0500

    made day 3 .py file, new day 1 part 2

--- day 1/day 1 part 2.py
+++ day 1/day 1 part 2.py
@@ -11,9 +11,11 @@ sums = [[depths[n], depths[n+1], depths[n+2]] for n, x in enumerate(depths[:-2])
 sums.append([depths[-2],depths[-1]])
 sums.append([depths[-1]])
 
+# do the thing
 for n, x in enumerate(sums):
     if sum(x) > sum(sums[n-1]):
         count += 1
+
 print(count)
 
 
