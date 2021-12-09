0197fa999ce00adbd1a06366ff5d71259253e075
Scott Ellis <scottellis94@gmail.com>
2021-12-09 01:16:37 -0500

    comments

--- day 5/day 5 part 1.py
+++ day 5/day 5 part 1.py
@@ -22,7 +22,7 @@ class LineSeg:
         """
         creates a line segment and returns a list of all points
         """
-        if self.x1 < self.x2:
+        if self.x1 < self.x2:   # check if pos or neg slope
             horiz = list(range(self.first_point[0], self.second_point[0] + 1))
         else:
             horiz = list(reversed(range(self.second_point[0], self.first_point[0] + 1)))
@@ -30,7 +30,7 @@ class LineSeg:
         if horiz is []:  # if range is 0
             horiz = self.first_point[0]
 
-        if self.y1 < self.y2:
+        if self.y1 < self.y2:  # check if pos or neg slope
             vert = list(range(self.first_point[1], self.second_point[1] + 1))
         else:
             vert = list(reversed(range(self.second_point[1], self.first_point[1] + 1)))
@@ -38,21 +38,18 @@ class LineSeg:
         if vert is []:  # if range is 0
             vert = self.first_point[0]
 
-        if len(horiz) > len(vert):
-            vert = vert * len(horiz)
+        if len(horiz) > len(vert):     # gen a list of n length coord if it doesn't change, will prob merge with
+            vert = vert * len(horiz)   # upper if statements later
         else:
             horiz = horiz * len(vert)
 
         return list(zip(horiz, vert))
 
 
-
-#all_lines = [LineSeg(x) for x in points_list]
+all_lines = [LineSeg(x) for x in points_list]
 
 test = LineSeg(points_list[3])
 print(test.line())
-
-
 
 """
 after this i want to check each line seg list of coords against all others to see if there's any overlap
