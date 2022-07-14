b8a4f5ca4eb111e4d636055e3846766246520d07
Scott Ellis <scottellis94@gmail.com>
2022-07-13 23:55:33 -0400

    Added format file, swap day 1 2015 to new format

---
+++ utils/format of day.py
@@ -0,0 +1,16 @@
+
+class Part1:
+    @staticmethod
+    def solution(parens: str):
+        pass
+
+
+class Part2:
+    @staticmethod
+    def solution(parens: str):
+        pass
+
+
+with open("input.txt", "r") as file:
+    print(Part1.solution(file.readline()))
+    print(Part2.solution(file.readline()))
