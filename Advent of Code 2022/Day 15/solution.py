feced4d6480b04578224d382258d525f096c7acd
Scott Ellis <scottellis94@gmail.com>
2022-07-15 23:28:03 -0400

    Day 2 2015, added correct typing in solution_template.py and rebuilt trees

--- Advent of Code 2022/Day 15/solution.py
+++ Advent of Code 2022/Day 15/solution.py
@@ -11,9 +11,10 @@ class Part2:
 
 
 with open("input.txt", "r") as file:
-    f = file.readlines()
-    print(f"Part 1: {Part1.solution(f)}")
-    print(f"Part 2: {Part2.solution(f)}")
+    f = file.read().splitlines()
+
+print(f"Part 1: {Part1.solution(f)}")
+print(f"Part 2: {Part2.solution(f)}")
 
 
 
--- utils/solution_template.py
+++ utils/solution_template.py
@@ -1,19 +1,20 @@
 class Part1:
     @staticmethod
-    def solution(file_lines: list) -> int:
+    def solution(file_lines: list[str]) -> int:
         pass
 
 
 class Part2:
     @staticmethod
-    def solution(file_lines: list) -> int:
+    def solution(file_lines: list[str]) -> int:
         pass
 
 
 with open("input.txt", "r") as file:
-    f = file.readlines()
-    print(f"Part 1: {Part1.solution(f)}")
-    print(f"Part 2: {Part2.solution(f)}")
+    f = file.read().splitlines()
+
+print(f"Part 1: {Part1.solution(f)}")
+print(f"Part 2: {Part2.solution(f)}")
 
 
 
