87bdcaa79229f36e22e4a7001faa4816c5c533b7
Scott Ellis <scottellis94@gmail.com>
2022-12-02 23:08:15 -0500

    solve day 2 2022

--- advent_of_code_2022/day_02/tests.py
+++ advent_of_code_2022/day_02/tests.py
@@ -5,24 +5,7 @@ with open("input.txt", "r") as file:
 
 
 def test_part1_1():
-    assert s.Part1.solution(['']) == 0
-
-
-def test_part1_2():
-    assert s.Part1.solution(['']) == 0
-
-
-def test_part1_3():
-    assert s.Part1.solution(['']) == 0
-
+    assert s.Part1.solution(['A Z']) == 3
 
 def test_part2_1():
-    assert s.Part2.solution(['']) == 0
-
-
-def test_part2_2():
-    assert s.Part2.solution(['']) == 0
-
-
-def test_part2_3():
-    assert s.Part2.solution(['']) == 0
+    assert s.Part2.solution(['A Z']) == 8
